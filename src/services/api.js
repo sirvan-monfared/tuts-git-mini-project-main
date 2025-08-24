46e94268e5ec2c1f045726600d18da8946559d9a
Sirvan Monfared <sirvan.monfareddev@gmail.com>
2025-08-24 18:15:36 +0330

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "eccb2a131c244c6bed1f2fb71a6fa069";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
