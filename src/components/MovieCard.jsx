47261aeee9b5bc71718f28aee0f8b2010f321415
Sirvan Monfared <sirvan.monfareddev@gmail.com>
2025-08-24 19:57:00 +0330

    added favorite feature logic

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,7 +15,7 @@ function MovieCard({movie}) {
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
-
+                <button onClick={onFavoriteClick}>add to favorite</button>
             </div>
         </div>
         <div className="movie-info">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import "../css/Navbar.css"
+import { useMovieContext } from "../contexts/MovieContext";
 
 function NavBar() {
+
+    const {favorites} = useMovieContext();
+
     return <nav className="navbar">
         <div className="navbar-brand">
             <Link to="/">Movie App</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
+
+            { favorites.length > 0 && <Link to="/favorites" className="nav-link">Favorites</Link> }
+            
         </div>
     </nav>
 }
